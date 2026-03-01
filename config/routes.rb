66bbee0b95801f5af94a791965aa0661c185a7f7
tania-macheieva <macheieva.tetiana@gmail.com>
2026-03-01 20:33:24 +0000

    refacrore: minor changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,8 @@
     root "books#index"
 
     resources :books do
-      resources :reviews, only: [:index, :create, :update, :destroy], shallow: true
+      resources :reviews, only: [ :index, :create, :update, :destroy ], shallow: true
     end
-
-    resources :reviews, only: [ :index, :create, :update, :destroy ]
 
     get "up" => "rails/health#show", as: :rails_health_check
   end
--- test/controllers/book_controller_test.rb
+++
@@ -1,8 +0,0 @@
-require "test_helper"
-
-class BookControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get books_url
-    assert_response :success
-  end
-end
