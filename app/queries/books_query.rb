cb0f90bd0dbb4b7fb8172163670de34df3297cd4
tania-macheieva <macheieva.tetiana@gmail.com>
2026-03-03 19:59:44 +0000

    fix: optimize book query

--- app/queries/books_query.rb
+++ app/queries/books_query.rb
@@ -1,33 +1,39 @@
 class BooksQuery
+  ALLOWED_SORT_COLUMNS = %w[title created_at average_rating].freeze
+
   def initialize(params, scope = Book.all)
     @params = params
     @scope = scope
   end
 
   def call
-    @scope.includes(reviews: :user)
-          .yield_self(&method(:search))
-          .yield_self(&method(:sort))
-          .yield_self(&method(:paginate))
+    scope = @scope.includes(reviews: :user)
+    scope = search(scope)
+    scope = sort(scope)
+    scope = paginate(scope)
+    scope
   end
 
   private
 
   def search(scope)
     return scope unless @params[:search].present?
+
     scope.search_by_title_and_author(@params[:search])
   end
 
   def sort(scope)
-    sort_column = %w[title created_at average_rating].include?(@params[:sort_by]) ? @params[:sort_by] : "created_at"
-    sort_direction = @params[:order] == "desc" ? "desc" : "asc"
-    scope.order(sort_column => sort_direction)
+    sort_by = ALLOWED_SORT_COLUMNS.include?(@params[:sortBy]) ? @params[:sortBy] : "created_at"
+    order = @params[:order] == "desc" ? :desc : :asc
+
+    scope.order(sort_by => order)
   end
 
   def paginate(scope)
     page = [@params[:page].to_i, 1].max
-    per  = @params[:per_page].present? ? @params[:per_page].to_i : 10
-    per = [[per, 1].max, 50].min  # мін 1, макс 50
-    scope.limit(per).offset((page - 1) * per)
+    per_page = @params[:per_page].present? ? @params[:per_page].to_i : 10
+    per_page = [[per_page, 1].max, 50].min
+
+    scope.limit(per_page).offset((page - 1) * per_page)
   end
 end
